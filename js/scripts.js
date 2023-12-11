07d8514e2a82169b932ba5247c1c422b134db84b
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2023-12-11 08:07:57 +0100

    Added second exercise

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ innerHTML.addEventListener('click', function() {
     const nombre = prompt('Introduzca su nombre')
     innerHTML.innerHTML = nombre
 })
+
+const textContent = document.querySelector('#textContent')
+textContent.addEventListener('click', function() {
+    const nombre = prompt('Introduzca su nombre')
+    textContent.textContent = nombre
+})
