3057b280c98d1bf233ceb204f81562804547a3e1
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2023-12-11 08:28:31 +0100

    Added button exercise

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,24 @@ textContent.addEventListener('click', function() {
     const nombre = prompt('Introduzca su nombre')
     textContent.textContent = nombre
 })
+
+const append = document.querySelector('#append')
+append.append('mundo')
+
+const prepend = document.querySelector('#prepend')
+prepend.prepend('Hola ')
+
+const value = document.querySelector('#value')
+value.addEventListener('click', function () {
+    const nombre = prompt('Introduzca su nombre')
+    value.value = nombre
+})
+
+const botonClickDobleClick = document.querySelector('#click-dblclick')
+botonClickDobleClick.addEventListener('click', function () {
+    botonClickDobleClick.value = 'Hice click'
+})
+
+botonClickDobleClick.addEventListener('dblclick', function () {
+    botonClickDobleClick.value = 'Hice doble click'
+})
