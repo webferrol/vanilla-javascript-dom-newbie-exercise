88c0c72dde80dbba0f413a889f8a55607cf0b0ba
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2023-12-12 13:06:28 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 const innerHTML = document.querySelector('#innerHTML')
-innerHTML.addEventListener('click', function() {
+innerHTML.addEventListener('click', function () {
     const nombre = prompt('Introduzca su nombre')
     innerHTML.innerHTML = nombre
 })
 
 const textContent = document.querySelector('#textContent')
-textContent.addEventListener('click', function() {
+textContent.addEventListener('click', function () {
     const nombre = prompt('Introduzca su nombre')
     textContent.textContent = nombre
 })
